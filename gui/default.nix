dfc850e8da27df41b9a3e95081e4f8cfc1303ed5
wjiangye <weihaojiangye@proton.me>
2026-09-23 18:52:32 +0200

    Add files via upload

---
+++ gui/default.nix
@@ -0,0 +1,97 @@
+{ config, pkgs, ... }: {
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [
+	pkgs.xdg-desktop-portal-gnome
+	pkgs.xdg-desktop-portal-gtk
+      ];
+
+      config.common.default = [ "gnome" "gtk" ];
+    };
+
+    environment = {
+      sessionVariables = {
+        NIXOS_OZONE_WL = "1";
+
+	QT_QPA_PLATFORM = "wayland;xcb";
+	QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+
+	GTK_BACKEND = "wayland,x11,*";
+	CLUTTER_BACKEND = "wayland";
+
+	SDL_VIDEODRIVER = "wayland";
+	_JAVA_AWT_WM_NONREPARENTING = "1";
+      };
+
+      systemPackages = with pkgs; [
+        xwayland-satellite
+        xwayland
+
+	swaylock
+	alacritty
+	fuzzel
+      ];
+    };
+
+    programs = {
+      niri.enable = true;
+
+      firefox = {
+	enable = true;
+	languagePacks = [ "es-ES" "de" ];
+
+	policies = {
+	  DisableTelemetry = true;
+	  DisableFirefoxStudies = true;
+	  DisablePocket = true;
+	  DisableFeedbackCommands = true;
+
+	  EnableTrackingProtection = {
+	    Value = true;
+	    Locked = true;
+	    Criptomining = true;
+	    Fingerprinting = true;
+	  };
+
+	  DontCheckDefaultBrowser = true;
+
+	  FirefoxHome = {
+	    Search = true;
+	    TopSites = false;
+	    SponsoredTopSites = false;
+	    Pocket = false;
+	    SponsoredPocket = false;
+	    Snippets = false;
+	  };
+
+	  ExtensionSettings = {
+	    "*".installation_mode = "blocked";
+
+	    "uBlock0@raymondhill.net" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+	      installation_mode = "force_installed";
+	    };
+
+	    "{1E6C5B3F-7D4E-4E8F-A9C8-8F8E8E8E8E8E}" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/descentraleyes/latest.xpi";
+	      installation_mode = "force_installed";
+	    };
+	  };
+
+	  UserPreferences = {
+	    "privacy.globalprivacycontrol.enabled" = true;
+	    "browser.newtabpage.activity-stream.showSponsored" = false;
+	    "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+	    "browser.ping-centre.telemetry" = false;
+	  };
+        };
+      };      
+    };
+}
+	
